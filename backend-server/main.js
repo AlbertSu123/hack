0f098a47007063ed2423f04fa64b2fe5b7f7ef52
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-10-16 12:03:38 -0700

    Change signer counts to continuous window of 1000

--- backend-server/main.js
+++ backend-server/main.js
@@ -9,31 +9,74 @@ import {
   setDefaultWasm,
 } from "@certusone/wormhole-sdk";
 
+import http from 'http';
+
+class Queue {
+  constructor() {
+    this.elements = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+  enqueue(element) {
+    this.elements[this.tail] = element;
+    this.tail++;
+  }
+  dequeue() {
+    const item = this.elements[this.head];
+    delete this.elements[this.head];
+    this.head++;
+    return item;
+  }
+  peek() {
+    return this.elements[this.head];
+  }
+  get length() {
+    return this.tail - this.head;
+  }
+  get isEmpty() {
+    return this.length === 0;
+  }
+}
+
 setDefaultWasm("node");
 
 const { parse_vaa } = await importCoreWasm();
 const client = createSpyRPCServiceClient("143.198.69.169:7073");
 const stream = await subscribeSignedVAA(client, {});
-import http from 'http'
+
+
+var hist = new Queue();
 
 var data = [];
 for (let i = 0; i < 19; i++) {
   data.push(0);
 }
-var counter = 0;
+
 stream.addListener("data", ({ vaaBytes }) => {
   const parsedVAA = parse_vaa(vaaBytes);
 
+  let curr = [];
+  for (let i = 0; i < 19; i++) {
+    curr.push(0);
+  }
+
   parsedVAA.signatures.forEach(obj=> {
-    counter+=1;
-    data[obj.guardian_index]+=1;
+    curr[obj.guardian_index] = 1;
+    data[obj.guardian_index] += 1;
   })
+
+  if (hist.length < 1000) {
+    hist.enqueue([...curr]);
+  } else {
+    hist.enqueue([...curr]);
+    let old = hist.dequeue();
+    for (let i = 0; i < 19; i++) {
+      data[i] -= old[i];
+    }
+  }
+
   //console.log(parsedVAA.signatures.length);
   console.log(data);
-  if (counter >=10000) {
-    data = data.map(x=>0);
-    counter = 0;
-  }
 
   //const payloadBuffer = Buffer.from(parsedVAA.payload);
   //const transferPayload = parseTransferPayload(payloadBuffer);
@@ -47,6 +90,4 @@ http.createServer(function (req, res) {
   const dataResponse = JSON.stringify(data)
   res.end(dataResponse);
 }).listen(9615);
-stream.addListener("error", (error) => {
-  console.log(error);
-});
+
